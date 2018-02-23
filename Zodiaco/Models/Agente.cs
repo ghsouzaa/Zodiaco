f49beecf5b0710c403fc42aa061450d684f253d2
Murillo Raggiotto <murillomr7@gmail.com>
2018-02-23 16:45:07 -0400

    comentários no caminhar()

--- Zodiaco/Models/Agente.cs
+++ Zodiaco/Models/Agente.cs
@@ -8,7 +8,9 @@ namespace Zodiaco.Models
     {
         List<Time> usadosBatalha = new List<Time>();
         public Time time;
+        // Objetivo é o número da casa a qual o char tem como alvo atual
         public char objetivoAtual = '1';
+        // O objetivoMapa guarda o mapa e retorna a posição da casa solicitada
         public Mapa objetivoMapa;
 
         public char[] objetivos = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K', 'E' };
@@ -35,57 +37,65 @@ namespace Zodiaco.Models
         {
             try
             {
+                // verifica se a posição eh uma casa de luta
                 if (atual.PosicaoLuta)
-                    atual.Esforco = Lutar(atual);
+                    atual.Esforco = Lutar(atual); //roda o Lutar p/ calcular o resultado da batalha
 
+                //verifica se um dos "filhos" (casas proximas, next step) é o seu objetivo
                 var objetivo = atual.Filhos.FirstOrDefault(x => objetivos.Contains(x.CharMapa));
                 if (objetivo != null)
                 {
                     caminho.Add(atual);
-                    if (objetivoAtual != 'E')
+                    if (objetivoAtual != 'E') //'E' é o ultimo objetivo, entra aki se não for o objetivo final
                     {
                         var indexObjetivo = Array.IndexOf(objetivos, objetivo.CharMapa);
                         objetivoAtual = objetivos[indexObjetivo + 1];
                     }
 
-                    if (objetivo.CharMapa == 'E')
+                    if (objetivo.CharMapa == 'E') // adiciona o ultimo passo, que é o objetivo final (casa do Gran Mestre)
                     {
                         caminho.Add(objetivo);
                         return caminho;
                     }
 
-                    objetivo.Filhos.Remove(atual);
+                    objetivo.Filhos.Remove(atual); 
 
                     return Caminhar(objetivo, map, caminho);
                 }
-                objetivoMapa = new Mapa().BuscarNoMapa(objetivoAtual, map);
 
+                // busca a proxima casa (objetivo), a posição da casa 
+                //objetivoMapa = new Mapa().BuscarNoMapa(objetivoAtual, map);
+
+                //retorna os caminhos
                 var filhosSemMontanhas = atual.Filhos.Where(x => x.CharMapa != 'M').ToList();
+                // filtra os caminhos que já foram percorridos
                 var filhosSemRepetir = filhosSemMontanhas.Except(caminho).ToList();
-                if (filhosSemRepetir.Count > 0)
-                    filhosSemRepetir = filhosSemRepetir.Except(caminhoExcluido).ToList();
 
-                if (filhosSemRepetir.Count == 0)
+                if (filhosSemRepetir.Count > 0)
+                    filhosSemRepetir = filhosSemRepetir.Except(caminhoExcluido).ToList(); //remove os caminhos que deram erro
+
+                if (filhosSemRepetir.Count == 0)// quando não tem mais para onde ir (da rollback)
                 {
                     caminhoExcluido.Add(atual);
-                    caminho.Remove(atual);
+                    caminho.Remove(atual); // faz rollback e volta recursivamente no caminho anterior
                     return Caminhar(caminho.Last(), map, caminho);
                 }
                 else
-                {
-                    caminho.Add(atual);
+                { //tem caminhos para percorrer
+                    caminho.Add(atual); //da o passo no caminho selecionado e ve o que que da!
 
-                    var menorEsforco = filhosSemRepetir.OrderBy(x => x.Esforco).First().Esforco;
-                    var todosMenor = filhosSemRepetir.Where(x => x.Esforco == menorEsforco).ToList();
+                    var menorEsforco = filhosSemRepetir.OrderBy(x => x.Esforco).First().Esforco; // calcula o menor esforço dos possíveis proximos passos
+                    var todosMenor = filhosSemRepetir.Where(x => x.Esforco == menorEsforco).ToList(); // seleciona/filtra os proximos passos com essa mesma qntd de esforço
 
+                    //se tiver mais de 1 'proximo passo' com o menor esforço
                     if (todosMenor.Count > 1)
                     {
-                        var filhoEscolhido = EscolherMelhorFilho(todosMenor);
+                        var filhoEscolhido = EscolherMelhorFilho(todosMenor); // escolhe o filho mais próximo do objetivo (casa)
                         return Caminhar(filhoEscolhido, map, caminho);
                     }
                     else
                     {
-                        var filhoEscolhido = todosMenor.OrderBy(x => x.Esforco).Last(); //MELHORAR AQUI
+                        var filhoEscolhido = todosMenor.OrderBy(x => x.Esforco).Last(); //escolhe o primeiro mais fácil (caminho)
                         filhoEscolhido.Filhos.Remove(atual);
                         return Caminhar(filhoEscolhido, map, caminho);
                     }
@@ -99,15 +109,17 @@ namespace Zodiaco.Models
 
         public double Lutar(Mapa casa)
         {
-            //return 0;
-
             var poderCasa = casa.Esforco;
 
             var poderEquipe = SelecionarEquipe(poderCasa);
 
             return poderCasa / poderEquipe;
         }
-
+        /// <summary>
+        /// Seleciona a equipe ideal de acordo com o Poder da Casa (adversário)
+        /// </summary>
+        /// <param name="poderCasa">Valor (ponto flutuante) do poder da casa</param>
+        /// <returns></returns>
         public double SelecionarEquipe(double poderCasa)
         {
             Cavaleiro primeiro, segundo, terceiro;
@@ -193,7 +205,7 @@ namespace Zodiaco.Models
                 Dictionary<Mapa, double> Posicao = new Dictionary<Mapa, double>();
                 foreach (var filho in filhos)
                 {
-                    var esforco = CalcularDistanciaFilho(filho, objetivoMapa);
+                    var esforco = CalcularDistanciaFilho(filho, objetivoMapa); //calcula matematicamente a coordenada na matrix que está mais próxima do objetivo
                     Posicao.Add(filho, esforco);
                 }
                 var melhor = Posicao.OrderBy(x => x.Value).First().Key;
